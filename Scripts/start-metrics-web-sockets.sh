6142dcebc64edba35643863458817c6e164ced4c
mariammartirosyan <mariam.martirosyan.156@gmail.com>
2024-06-26 13:02:44 +0200

    docs: update presentation slides

--- Scripts/start-metrics-web-sockets.sh
+++ Scripts/start-metrics-web-sockets.sh
@@ -32,7 +32,7 @@ else
 fi
 
 echo "Creating the namespace"
-apply_kubectl "../Grafana/00-metrics-namespace.yaml"
+apply_kubectl "../Grafana/metrics-namespace.yaml"
 
 echo "Deploying Prometheus"
 
@@ -49,13 +49,5 @@ helm install prometheus prometheus-community/kube-prometheus-stack -n metrics
 
 apply_kubectl "../WebSockets/Manifest Files/trailer-streaming-web-socket-monitor.yaml" 
 
-# apply_kubectl basic_services/jobs/outer_jobs/db_synchronizer/prod/03-db-synchronizer-monitor.prod.yaml
-
 echo "Deploying Grafana"
-apply_kubectl "../Grafana/00-grafana-pvc.prod.yaml"
-apply_kubectl "../Grafana/01-grafana-datasources.prod.yaml"
-apply_kubectl "../Grafana/02-grafana-deployment.prod.yaml"
-apply_kubectl "../Grafana/03-grafana-service.prod.yaml"
-
-
-#apply_kubectl "../REST/Manifest Files/trailer-streaming-rest-monitor.yaml" 
+apply_kubectl "../Grafana/grafana.yaml"
